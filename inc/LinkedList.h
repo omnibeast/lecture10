a8947a0b5197cc8448de6113c72078634d416d69
Saurav Pokharel <78597546+omnibeast@users.noreply.github.com>
2026-03-22 22:27:56 -0600

    git commit -m completed

--- inc/LinkedList.h
+++ inc/LinkedList.h
@@ -6,5 +6,83 @@
 using namespace std;
 
 //TODO: Create your Node Struct and Queue class here
+struct Node {
+    string data;
+    Node* next;
+
+    Node(string value) : data(value), next(nullptr) {}
+};
+
+class Queue {
+private:
+    Node* head;
+    Node* tail;
+    int count;
+public:
+    Queue() : head(nullptr), tail(nullptr), count(0) {}
+
+    ~Queue() {
+        while (!isEmpty()) {
+            dequeue();
+        }
+    }
+
+    void enqueue(const string& value) {
+        Node* newNode = new Node(value);
+        if (isEmpty()) {
+            head = tail = newNode;
+        }
+        else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        count++;
+    }
+
+    void dequeue() {
+        if (isEmpty()) {
+            return;
+        }
+        Node* oldHead = head;
+        head = head->next;
+        delete oldHead;
+        count--;
+        if (head == nullptr) {
+            tail = nullptr;
+        }
+    }
+
+    string peek() const {
+        if (isEmpty()) {
+            return "";
+        }
+        return head->data;
+    }
+
+    int size() const {
+        return count;
+    }
+
+    bool isEmpty() const {
+        return count == 0;
+    }
+
+    void print() const {
+        if (isEmpty()) {
+            cout << "Queue is empty." << endl;
+            return;
+        }
+        Node* current = head;
+        while (current != nullptr) {
+            cout << current->data;
+            if (current->next != nullptr) {
+                cout << " -> ";
+            }
+            current = current->next;
+        }
+        cout << endl;
+
+    }
+};
 
 #endif
